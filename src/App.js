bea39f745a7eb8cf43f5ba923ef757239d694a52
absamie <abdulsamie818@gmail.com>
2020-06-18 01:15:20 +0200

    first push from linux terminal

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Message from './Message.js';
+import './App.css';
+
+function App() {
+let [count, setCount] = useState(1)  
+let [isMorning, setMorning] = useState(false) 
+return (
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+
+      <h1>Good  {isMorning? 'Morning' : 'Night'}</h1>
+    <Message counter = {count}/>
+    <br/>
+    <button onClick={
+  () => setCount(++count)
+  }>
+    Update Counter
+    </button>
+    <button onClick = {() => setMorning(!isMorning)}>Update day</button>
+    </div>
+  );
+}
+
+export default App;
